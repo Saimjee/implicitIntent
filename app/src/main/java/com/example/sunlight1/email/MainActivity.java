285e4b1b7e1ce3d985ce4afc888f50b4b30e095c
sunlight1 <awesomesaim@gmail.com>
2017-12-19 17:12:01 +0500

    Initial commit

---
+++ app/src/main/java/com/example/sunlight1/email/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.sunlight1.email;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn= (Button) findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Intent.ACTION_SEND);
+                intent.setData(Uri.parse("Email"));
+                Intent chooser=Intent.createChooser(intent,"");
+                startActivity(chooser);
+            }
+        });
+    }
+}
